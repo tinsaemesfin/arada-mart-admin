6dd617f50cda044916695d51341b8a88b9e6634c
tinsaemesfin <qmesfin@gmail.com>
2023-07-20 09:48:34 +0300

    auth done with clerk

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+export default function SetupPage() {
+    return (
+      <div className="p-4">
+        <UserButton afterSignOutUrl="/"/>
+        </div>
+    )
+  }
+  
