87c95d7e64cbb7c860bf0ba78c7f224f6137fdec
tinsaemesfin <qmesfin@gmail.com>
2023-08-03 13:45:12 +0300

    fix bug in sizes

--- app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/components/sizes-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/components/sizes-form.tsx
@@ -14,7 +14,7 @@ import { Heading } from "@/components/ui/heading";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Billboard, Size } from "@prisma/client";
+import {  Size } from "@prisma/client";
 import { Trash } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { use, useState } from "react";
@@ -22,7 +22,6 @@ import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { useOrigin } from "@/hooks/use-origin";
 import * as z from "zod";
-import ImageUpload from "@/components/ui/image-upload";
 import axios from "axios";
 
 interface SizeClientProps {
